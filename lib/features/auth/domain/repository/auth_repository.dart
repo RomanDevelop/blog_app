e325841c30f63f90c560aeea8db7114b5fc18f55
roman.k <roman.k@3sda-evayadatasystems.com>
2024-03-10 16:52:45 +0100

    supabase init completed

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,16 @@
+import 'package:blog_app/core/error/failures.dart';
+// import 'package:blog_app/core/common/entities/user.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> currentUser();
+}

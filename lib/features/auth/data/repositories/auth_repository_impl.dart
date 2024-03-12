6f25852c17c5d3c9da0bccb067bd88a582bd327b
roman.k <roman.k@3sda-evayadatasystems.com>
2024-03-12 11:12:52 +0100

    bloc for auth done

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,92 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+  //final ConnectionChecker connectionChecker;
+  const AuthRepositoryImpl(
+    this.remoteDataSource,
+    // this.connectionChecker,
+  );
+
+  // @override
+  // Future<Either<Failure, User>> currentUser() async {
+  //   try {
+  //     if (!await (connectionChecker.isConnected)) {
+  //       final session = remoteDataSource.currentUserSession;
+
+  //       if (session == null) {
+  //         return left(Failure('User not logged in!'));
+  //       }
+
+  //       return right(
+  //         UserModel(
+  //           id: session.user.id,
+  //           email: session.user.email ?? '',
+  //           name: '',
+  //         ),
+  //       );
+  //     }
+  //     final user = await remoteDataSource.getCurrentUserData();
+  //     if (user == null) {
+  //       return left(Failure('User not logged in!'));
+  //     }
+
+  //     return right(user);
+  //   } on ServerException catch (e) {
+  //     return left(Failure(e.message));
+  //   }
+  // }
+
+  // @override
+  // Future<Either<Failure, String>> loginWithEmailPassword({
+  //   required String email,
+  //   required String password,
+  // }) async {
+  //   return _getUser(
+  //     () async => await remoteDataSource.loginWithEmailPassword(
+  //       email: email,
+  //       password: password,
+  //     ),
+  //   );
+  // }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    // return _getUser(
+    //   () async => await remoteDataSource.signUpWithEmailPassword(
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
+
+//   Future<Either<Failure, User>> _getUser(
+//     Future<User> Function() fn,
+//   ) async {
+//     try {
+//       if (!await (connectionChecker.isConnected)) {
+//         return left(Failure(Constants.noConnectionErrorMessage));
+//       }
+//       final user = await fn();
+
+//       return right(user);
+//     } on ServerException catch (e) {
+//       return left(Failure(e.message));
+//     }
+//   }
+//}

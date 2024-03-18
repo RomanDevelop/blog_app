f8ea7d7922b560d10e3ea2dd70e6b4d43ddc73e9
roman.k <roman.k@3sda-evayadatasystems.com>
2024-03-18 21:04:18 +0100

    blog presentation in progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:blog_app/core/theme/lib/core/theme/theme.dart';
+import 'package:blog_app/features/blog/presentation/pages/blog_page.dart';
 import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -40,7 +41,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // 3.44
+      debugShowCheckedModeBanner: false, // 4.22
       title: 'Flutter Demo',
       theme: AppTheme.darkThemeMode,
       home: BlocSelector<AppUserCubit, AppUserState, bool>(
@@ -49,12 +50,7 @@ class _MyAppState extends State<MyApp> {
         },
         builder: (context, isLoggedIn) {
           if (isLoggedIn) {
-            return const Scaffold(
-              body: Center(
-                child: Text('Logged in!'),
-              ),
-            );
-            //   return const BlogPage();
+            return const BlogPage();
           }
           return const LoginPage();
         },

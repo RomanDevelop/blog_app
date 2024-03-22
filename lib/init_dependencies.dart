572d896cafb6536e5820aaa46aa42ced4e4d4534
roman.k <roman.k@3sda-evayadatasystems.com>
2024-03-22 20:38:48 +0100

    local db, di done

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -8,6 +8,7 @@ import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
@@ -15,7 +16,9 @@ import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
@@ -29,13 +32,13 @@ Future<void> initDependencies() async {
     anonKey: AppSecrets.supabaseAnonKey,
   );
 
-  //Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
 
   serviceLocator.registerLazySingleton(() => supabase.client);
 
-  // serviceLocator.registerLazySingleton(
-  //   () => Hive.box(name: 'blogs'),
-  // );
+  serviceLocator.registerLazySingleton(
+    () => Hive.box(name: 'blogs'),
+  );
 
   serviceLocator.registerFactory(() => InternetConnection());
 
@@ -100,17 +103,17 @@ void _initBlog() {
         serviceLocator(),
       ),
     )
-    // ..registerFactory<BlogLocalDataSource>(
-    //   () => BlogLocalDataSourceImpl(
-    //     serviceLocator(),
-    //   ),
-    // )
+    ..registerFactory<BlogLocalDataSource>(
+      () => BlogLocalDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
     // Repository
     ..registerFactory<BlogRepository>(
       () => BlogRepositoryImpl(
         serviceLocator(),
-        //  serviceLocator(),
-        //  serviceLocator(),
+        serviceLocator(),
+        serviceLocator(),
       ),
     )
     // Usecases
